d734228e71176a28bf51eb556365c020d9dc99f4
Niserg <31318028+Niserg@users.noreply.github.com>
2025-06-10 16:52:13 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/dds-website' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/dds-website/' : '',
   images: {
     unoptimized: true,
   },
+  basePath: process.env.NODE_ENV === 'production' ? '/dds-website' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/dds-website/' : '',
   trailingSlash: true,
 }
 

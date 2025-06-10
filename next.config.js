09e1b661f60025c95638d8ae4909e507836eef6d
niserg <niserg@igoymrak.local>
2025-06-10 17:16:44 +0530

    Fix merge conflicts and update configuration

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   images: {
+//     unoptimized: true,
+//   },
+//   basePath: process.env.NODE_ENV === 'production' ? '/DDS' : '',
+//   assetPrefix: process.env.NODE_ENV === 'production' ? '/DDS/' : '',
+//   trailingSlash: true,
+// }
+
+// module.exports = nextConfig 
+
+// above for local hosting
+// below for github
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
@@ -9,4 +25,4 @@ const nextConfig = {
   trailingSlash: true,
 }
 
-module.exports = nextConfig 
+module.exports = nextConfig
